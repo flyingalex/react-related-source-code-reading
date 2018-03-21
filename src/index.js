5fed7d06d2423a3047b23a4971c258c74904066f
flyingalex <hulinalex@163.com>
2018-03-21 23:23:53 +0800

    add @babel/polyfill for await/async

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+// await/async, etc
+require("@babel/polyfill");
+
 const add1 = (a) => (a + 1);
 
 // https://github.com/tc39/proposal-pipeline-operator
-const r = 2 
+const r = 2
   |> add1;
 
 console.log('pipeline in js', r);
