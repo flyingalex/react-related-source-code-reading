fa4350a1a526cf460a81a7631276bb713db2f8da
flyingalex <flyingalex@163.com>
2020-03-07 10:28:00 +0800

    remove useless package

--- src/index.jsx
+++ src/index.jsx
@@ -1,41 +1,14 @@
-import React from 'react-dev/react';
-import { render } from 'react-dev/react-dom';
-import {ThemeContext, themes} from './pages/theme-context';
-import ThemedButton from './pages/themed-button';
-
+import React from './react-dev/react';
+import { render } from './react-dev/react-dom';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      theme: themes.light,
-    };
-
-    this.toggleTheme = () => {
-      console.log('state.theme', this.state.theme);
-      this.setState(state => ({
-        theme:
-          state.theme === themes.dark
-            ? themes.light
-            : themes.dark,
-      }));
-    };
-
-    this.state = {
-      theme: themes.light,
-      toggleTheme: this.toggleTheme,
-    };
   }
-
-
   render() {
-    // The ThemedButton button inside the ThemeProvider
-    // uses the theme from state while the one outside uses
-    // the default dark theme
+    console.log('render');
     return (
-      <ThemeContext.Provider value={this.state}>
-        <ThemedButton />
-      </ThemeContext.Provider>
+      <div>hello world</div>
     );
   }
 }
